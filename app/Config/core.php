1ce2690a875ea9a4438d8068fdb90744491bfb4d
Yasam Bingol <yasam.bingol@gmail.com>
2012-04-24 22:00:57 +0300

    security changes.

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,7 +184,8 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	//Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+  Configure::write('Security.salt', '123G93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
