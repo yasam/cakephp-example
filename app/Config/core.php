2c2fb532e6edaf31b54c788c4eacd4ef7a8e0e95
Yasam Bingol <yasam.bingol@gmail.com>
2012-04-24 22:07:59 +0300

    sec issues.

--- app/Config/core.php
+++ app/Config/core.php
@@ -190,7 +190,8 @@
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	//Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+  Configure::write('Security.cipherSeed', '12359309657453542496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
