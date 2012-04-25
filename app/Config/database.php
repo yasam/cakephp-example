7ba3198000edbff3a8c8e3b23c00f8b0dca7c6f3
Yasam Bingol <yasam.bingol@gmail.com>
2012-04-25 14:23:35 +0300

    bootstrap

--- app/Config/database.php
+++ app/Config/database.php
@@ -64,7 +64,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'root',
 		'database' => 'cake_2_0',
 		'prefix' => '',
 		//'encoding' => 'utf8',
